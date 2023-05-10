89646bf53f5ac5eae2417a1482889cbf6b42baef
Adsandro Galindo Leite Filho <112688705+galindoptbr@users.noreply.github.com>
2023-05-11 00:14:58 +0100

    estudos 10 05

---
+++ mochila-de-viagem/mochila-de-viagem/js/estudos.js
@@ -0,0 +1,12 @@
+function soma (num1, num2) {
+    return num1 + num2;
+}
+
+console.log(soma(2, 3));
+
+function ordemAlfabetica(texto) {
+    return texto.split('').sort().join('');
+
+}
+
+console.log(ordemAlfabetica('Amor e odio'));
