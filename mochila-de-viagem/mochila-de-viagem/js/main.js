aaeefe9379de7ad2291aa54044851ddf948578c5
Adsandro Galindo Leite Filho <112688705+galindoptbr@users.noreply.github.com>
2023-05-15 21:54:33 +0100

    estudos 15 05

--- mochila-de-viagem/mochila-de-viagem/js/main.js
+++ mochila-de-viagem/mochila-de-viagem/js/main.js
@@ -1,70 +1,82 @@
-// uma constante "form" que representa o formulário com o ID
-const form = document.getElementById("novoItem") 
-//  uma constante "lista" que representa a lista de itens com o ID
+const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-const itens = JSON.parse(localStorage.getItem('itens')) || []
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+} )
 
-itens.forEach(element => {
-    criaElemento(element)
-});
-
-// ouvinte de evento "submit" ao formulário
 form.addEventListener("submit", (evento) => {
-    // método "preventDefault()" no evento passado como argumento para impedir que a página seja atualizada quando o formulário é enviado.
     evento.preventDefault()
 
-    const nome = evento.target.elements['nome'];
-    const quantidade = evento.target.elements['quantidade'];
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
 
-    const existe = itens.find(element => element.nome === nome.value)
+    const existe = itens.find( elemento => elemento.nome === nome.value )
 
     const itemAtual = {
-        'nome': nome.value,
-        'quantidade': quantidade.value
+        "nome": nome.value,
+        "quantidade": quantidade.value
     }
 
     if (existe) {
         itemAtual.id = existe.id
 
         atualizaElemento(itemAtual)
+//Refatoração da condicional if else, atualizando um id para cada item
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     } else {
-        itemAtual.id = itens.length
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
 
         criaElemento(itemAtual)
 
         itens.push(itemAtual)
     }
-    
-    // é chamada, passando os valores inseridos nos campos de nome e quantidade do formulário como argumentos.
 
-    localStorage.setItem('itens', JSON.stringify(itens))
+    localStorage.setItem("itens", JSON.stringify(itens))
 
-    nome.value = '';
-    quantidade.value = '';
-
-
+    nome.value = ""
+    quantidade.value = ""
 })
-    
-    // cria um novo elemento "li" para representar o novo item na lista, adiciona a classe "item" ao elemento e adiciona um elemento "strong" para mostrar a quantidade do item.
+
 function criaElemento(item) {
-    const novoItem = document.createElement('li')
+    const novoItem = document.createElement("li")
     novoItem.classList.add("item")
 
-    const numeroItem = document.createElement('strong')
-    // // O valor de "quantidade" é adicionado ao elemento "strong" usando o método "innerHTML".
+    const numeroItem = document.createElement("strong")
     numeroItem.innerHTML = item.quantidade
     numeroItem.dataset.id = item.id
-
-    // O valor de "nome" é adicionado ao elemento "li" usando o operador "+=" para concatenar o valor ao conteúdo HTML existente.
     novoItem.appendChild(numeroItem)
+
     novoItem.innerHTML += item.nome
 
-    // o novo item é adicionado à lista usando o método "appendChild()"
-    lista.appendChild(novoItem)
+    novoItem.appendChild(botaoDeleta(item.id)) // Referenciar a função botaoDeleta no nó da função principal
 
+    lista.appendChild(novoItem)
 }
 
 function atualizaElemento(item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+//Função para criar botão com evento de click nos itens, e retornar os itens clicados
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+//Função para deletar os itens enviados da função botaoDeleta no array de itens e no navegador
+
+function deletaElemento(tag, id) { 
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
