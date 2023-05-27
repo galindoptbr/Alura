77d2a0083e8eec20550ecd8f25154c39e7c00ba1
Adsandro Galindo Leite Filho <112688705+galindoptbr@users.noreply.github.com>
2023-05-27 22:39:33 +0100

    finished

--- js-consumindo-dados-api-main/main.js
+++ js-consumindo-dados-api-main/main.js
@@ -1,12 +1,30 @@
-var consultaCep = fetch('https://viacep.com.br/ws/03244080/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if (r.erro) {
-        throw Error('Esse CEP nao existe!')
-    } else 
-         console.log(r)
-})
-.catch(erro => console.log(erro));
+async function buscarendereco(cep) {
+    var mensagemErro = document.querySelector('#erro');
+    mensagemErro.innerHTML = '';
+    try {
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCEPConvertida = await consultaCep.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP nao existente!');
+        }
+        var endereco = document.querySelector('#endereco');
+        var bairro = document.querySelector('#bairro');
+        var cidade = document.querySelector('#cidade');
+        var estado = document.querySelector('#estado');
 
+        endereco.value = consultaCEPConvertida.logradouro;
+        cidade.value = consultaCEPConvertida.localidade;
+        bairro.value = consultaCEPConvertida.bairro;
+        estado.value = consultaCEPConvertida.uf;
 
-console.log(consultaCep);
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>*CEP invalido. Tente novamente!</p>`;
+        console.log(erro)
+    }
+
+}
+
+var cep = document.querySelector('#cep');
+cep.addEventListener('focusout', () => buscarendereco(cep.value));
